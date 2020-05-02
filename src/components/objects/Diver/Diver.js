e0d4befe26986e3efef9b9ca5c740b451a3aef37
Vinay <vramesh@princeton.edu>
2020-05-02 15:26:42 -0400

    diver correctly oriented

--- src/components/objects/Diver/Diver.js
+++ src/components/objects/Diver/Diver.js
@@ -1,6 +1,6 @@
 import { Group } from 'three';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
-import MODEL from './minecraft-steve.gltf';
+import MODEL from './diver.gltf';
 
 class Diver extends Group {
     constructor() {
