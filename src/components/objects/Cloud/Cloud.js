29bdf916695a348f09ce2d3403ea1737324db342
Zhongqiao Gao <19952817+gaoz15@users.noreply.github.com>
2020-05-04 23:10:54 -0400

    fix cloud position

--- src/components/objects/Cloud/Cloud.js
+++ src/components/objects/Cloud/Cloud.js
@@ -16,15 +16,14 @@ class Cloud extends Object3D {
         this.scaleFactor = Math.random()*0.1;
 
         loader.load(MODEL, (gltf) => {
-            gltf.scene.position.x = parent.diver.position.x - Math.floor(Math.random()*10-5);
-            gltf.scene.position.y = parent.diver.position.y - Math.floor(Math.random()*80);
-            gltf.scene.position.z = parent.diver.position.z - Math.floor(Math.random()*10-5);
+            gltf.scene.position.x = parent.diver.position.x - Math.floor(Math.random()*10);
+            gltf.scene.position.y = parent.diver.position.y - Math.floor(Math.random()*70);
+            gltf.scene.position.z = parent.diver.position.z - Math.floor(Math.random()*10);
             gltf.scene.scale.set(this.scaleFactor, this.scaleFactor, this.scaleFactor);
 
             this.box = new Box3().setFromObject( gltf.scene );
             this.center = this.box.getCenter( new Vector3() );
 
-            console.log(this.center);
             this.box = new Box3().setFromObject( gltf.scene );
             var helper = new Box3Helper( this.box, 0xffff00 );
             this.add( helper );
